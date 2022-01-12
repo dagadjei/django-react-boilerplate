772c9b7716edbe69cf2d2e45278d189d8d83f759
Dag Adjei <adjeidag@gmail.com>
2022-01-12 22:18:31 +0000

    improved comments

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Signup from "./containers/Signup";
 import HomepageLayout from "./containers/Home";
 import ProductList from "./containers/ProductList";
 
-// react routes with types of views
+// react routes with types of views. 
 const BaseRouter = () => (
   <Hoc>
     <Route path="/products" component={ProductList} />
